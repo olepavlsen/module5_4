10bf5a937707ab208bb6284f40fc5ad7abe35d03
olepavlsen <olepavlsen@mail.ru>
2024-08-15 22:32:41 +0300

    Add files via upload

--- module_5_4.py
+++ module_5_4.py
@@ -2,12 +2,12 @@ class House:
     houses_history = []
 
     def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(str(args[0]))
         return object.__new__(cls)
 
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-        self.houses_history.append(self.name)
 
     new_floor = int
 
